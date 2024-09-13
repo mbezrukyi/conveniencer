33b16027f02e005cb98c675c80a47ae8735771a6
Mykola Bezrukyi <kolya.bezrukiy.03@gmail.com>
2024-09-13 18:56:37 +0300

    add local MongoDB and book logic

--- src/conveniencer/handlers/callback_queries/categories.py
+++ src/conveniencer/handlers/callback_queries/categories.py
@@ -3,6 +3,8 @@ from aiogram.types import CallbackQuery, Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.utils.formatting import Italic, Text
+from motor.motor_asyncio import AsyncIOMotorDatabase
+from pymongo.errors import DuplicateKeyError
 
 from conveniencer.filters.document_type import DocumentTypeFilter
 from ..callback_data import (
@@ -19,13 +21,15 @@ router = Router(name=__name__)
 class Action(StatesGroup):
     book = State()
     add_book = State()
+    remove_book = State()
 
 
 @router.callback_query(CallbackCategory.filter(F.category == Category.BOOKS))
 async def handle_books_category(
     query: CallbackQuery,
-    state: FSMContext,
     bot: Bot,
+    state: FSMContext,
+    db: AsyncIOMotorDatabase,
 ) -> None:
     await state.set_state(Action.book)
 
@@ -37,39 +41,31 @@ async def handle_books_category(
     # processor.remove()
     # processor.as_list()
 
-    # TODO: Display list of already added books
+    books = await db.books.find().to_list(length=None)
 
-    # books = await mongo.books.find()
+    chat_id = query.message.chat.id
 
-    # chat_id = query.message.chat.id
+    if books:
+        await bot.send_message(chat_id=chat_id, text="Your books:")
 
-    # if books:
-    #     await bot.send_message(chat_id=chat_id, text="Your books:")
-    #
-    #     async for book in books:
-    #         await bot.send_document(
-    #             chat_id=chat_id,
-    #             document=book.file_id,
-    #             caption=book.name,
-    #         )
-    #
-    #     await bot.send_message(
-    #         chat_id=chat_id,
-    #         text="What do you want to do?",
-    #         reply_markup=build_add_remove_keyboard(),
-    #     )
-    # else:
-    #     await bot.send_message(
-    #         chat_id=chat_id,
-    #         text="Add your first book",
-    #         reply_markup=build_add_remove_keyboard(),
-    #     )
+        for book in books:
+            await bot.send_document(
+                chat_id=chat_id,
+                document=book["file_id"],
+                caption=book["name"],
+            )
 
-    await bot.send_message(
-        chat_id=query.message.chat.id,
-        text="Your books:",
-        reply_markup=build_add_remove_keyboard(),
-    )
+        await bot.send_message(
+            chat_id=chat_id,
+            text="What do you want to do?",
+            reply_markup=build_add_remove_keyboard(),
+        )
+    else:
+        await bot.send_message(
+            chat_id=chat_id,
+            text="Add your first book",
+            reply_markup=build_add_remove_keyboard(),
+        )
 
 
 @router.callback_query(
@@ -78,8 +74,8 @@ async def handle_books_category(
 )
 async def handle_add_book(
     query: CallbackQuery,
-    state: FSMContext,
     bot: Bot,
+    state: FSMContext,
 ) -> None:
     await state.set_state(Action.add_book)
 
@@ -101,25 +97,59 @@ async def handle_add_book(
     F.document,
     DocumentTypeFilter(".pdf"),
 )
-async def add_book(message: Message, state: FSMContext) -> None:
-    # TODO: Write the logic to add the book
+async def add_book(
+    message: Message,
+    state: FSMContext,
+    db: AsyncIOMotorDatabase,
+) -> None:
+    name = message.caption
+    file_id = message.document.file_id
 
-    # name = message.caption
-    # file_id = message.document.file_id
-
-    # try:
-    #     collection.insert_one(
-    #         {
-    #             "name": name,
-    #             "file_id": file_id,
-    #         },
-    #     )
-    # except SomeError:
-    #     collection.update_one(
-    #         {"name": name},
-    #         {"$set": {"file_id": file_id}},
-    #     )
+    try:
+        await db.books.insert_one(
+            {
+                "name": name,
+                "file_id": file_id,
+            },
+        )
+    except DuplicateKeyError:
+        await db.books.update_one(
+            {"name": name},
+            {"$set": {"file_id": file_id}},
+        )
 
     await message.answer("You've successfully added a new book!")
+
+    await state.clear()
+
+
+@router.callback_query(
+    Action.book,
+    CallbackCategoryAction.filter(F.category_action == CategoryAction.REMOVE),
+)
+async def handle_remove_book(
+    query: CallbackQuery,
+    bot: Bot,
+    state: FSMContext,
+) -> None:
+    await state.set_state(Action.remove_book)
+
+    await bot.send_message(
+        chat_id=query.message.chat.id,
+        text="Specify a book name to delete",
+    )
+
+
+@router.message(Action.remove_book, F.text)
+async def remove_book(
+    message: Message,
+    state: FSMContext,
+    db: AsyncIOMotorDatabase,
+) -> None:
+    name = message.text
+
+    await db.books.delete_one({"name": name})
+
+    await message.answer(f"You've successfully delete the {name} book!")
 
     await state.clear()
--- src/conveniencer/main.py
+++ src/conveniencer/main.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from conveniencer import handlers
 from conveniencer.config import load_config
@@ -8,9 +9,12 @@ from conveniencer.config import load_config
 async def main() -> None:
     config = load_config(".bot")
 
+    mongo = AsyncIOMotorClient("localhost", 27017)
+    db = mongo.conviniencer
+
     bot = Bot(token=config.bot.token)
 
-    dp = Dispatcher()
+    dp = Dispatcher(db=db)
     dp.include_routers(
         handlers.router,
     )
